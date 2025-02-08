ae4833ac43d879cfbbe97024501e3d9b9601539a
eacastel <emilio@aliigo.com>
2025-02-08 04:31:04 -0800

    more for osano gdpr 4

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,13 +4,3 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-import './src/styles/globals.css'
-
-import React from "react";
-
-export const onInitialClientRender = () => {
-  const script = document.createElement("script");
-  script.src = "https://cmp.osano.com/yxyO4yMBFs/b090757f-6ce3-4269-b8c8-73e249743538/osano.js";
-  script.async = true;
-  document.body.appendChild(script);
-};
