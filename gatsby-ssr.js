dc6095a6305656968da488a77222f41f340f25c7
eacastel <emilio@aliigo.com>
2025-02-08 05:50:03 -0800

    more for cookieyes gdpr 9

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,9 +10,11 @@ import React from "react";
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <script
-      key="ccm19"
-      src="https://cloud.ccm19.de/app.js?apiKey=9366fea2036d5a10c5aa3cc29e952b3b2d35aa073de2e93e&amp;domain=67a75db7f438bb3eb40bc7a2"
-      referrerPolicy="origin"
+      key="cookieyes"
+      id="cookieyes"
+      type="text/javascript"
+      src="https://cdn-cookieyes.com/client_data/8a1fc6aec17f5166519ee614/script.js"
     />,
   ]);
 };
+
