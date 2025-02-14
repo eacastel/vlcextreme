1cbbabdf0f68eed3bf70f2651eff69fdf7041fb8
eacastel <emilio@aliigo.com>
2025-02-14 01:45:42 -0800

    font fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,16 +10,7 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
       src="https://cdn-cookieyes.com/client_data/8a1fc6aec17f5166519ee614/script.js"
       defer
     />,
-    // Preload Fonts and Styles
-    <link key="preload-font" rel="preload" href="/path-to-your-font.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />,
-    <link key="preload-css" rel="preload" href="/styles.css" as="style" />,
-    <style key="font-swap">
-      {`
-        body { font-display: swap; }
-      `}
-    </style>,
   ]);
-
   setPostBodyComponents([
     // Cloudflare Email Decode Script
     <script
@@ -29,5 +20,4 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
       defer
     />,
   ]);
-  
 };
