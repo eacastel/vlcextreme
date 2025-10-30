a7515082e6203d5d8bf7023141d0add0ceeba33c
eacastel <emilio@aliigo.com>
2025-10-30 03:58:19 -0700

    move cookieyes to GTM

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,14 +1,1 @@
-import React from "react";
-
-export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: 'es' });
-  setHeadComponents([
-    <script
-      key="cookieyes"
-      id="cookieyes"
-      type="text/javascript"
-      src="https://cdn-cookieyes.com/client_data/8a1fc6aec17f5166519ee614/script.js"
-      defer
-    />,
-  ]);
-};
+export const onRenderBody = () => {};
